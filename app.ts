d6fddaea40b21f593bd76bb1d594ad89a2be3230
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-06-06 11:30:02 +0100

    more setup

--- app.ts
+++ app.ts
@@ -1,1 +1,49 @@
-import express, {Application} from 'express'
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
+import compression from 'compression';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import Controller from '@/utils/interfaces/controller.interfaces';
+import ErrorMiddleware from '@/middleware/error.middleware';
+
+class App {
+  public express: Application;
+  public port: number;
+
+  constructor(controllers: Controller[], port: number) {
+    this.express = express();
+    this.port = port;
+
+    this.initialiseMiddleware();
+    this.initialiseControllers(controllers);
+    this.initialiseErrorHandling();
+  }
+
+  private initialiseMiddleware(): void {
+    this.express.use(morgan('combined'));
+    this.express.use(helmet());
+    this.express.use(cors());
+    this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: false }));
+    this.express.use(compression());
+  }
+
+  private initialiseControllers(controllers: Controller[]): void {
+    controllers.forEach((controller: Controller) => {
+      this.express.use('/api', controller.router);
+    });
+  }
+
+  private initialiseErrorHandling() {
+    this.express.use(ErrorMiddleware);
+  }
+
+  public listen(): void {
+    this.express.listen(this.port, () => {
+      console.log(`Server Listening on Port ${this.port}`);
+    });
+  }
+}
+
+export default App;
